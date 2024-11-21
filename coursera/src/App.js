2bcf6aa9440e99da82f2483ebad8d1a95d606cb4
ekaitzbustork <e.busto@orekait.com>
2024-11-21 11:26:25 +0100

    Commit 2

--- coursera/src/App.js
+++ coursera/src/App.js
@@ -2,9 +2,50 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hola
-    </div>
+    <>
+
+    <Header>
+      <Nav>
+        <img></img>
+        <ul>
+          <li>
+            <a>
+              HOME
+            </a>
+          </li>
+          <li>
+            <a>
+              ABOUT
+            </a>
+          </li>
+          <li>
+            <a>
+              RESERVATIONS
+            </a>
+          </li>
+          <li>
+            <a>
+              ORDER ONLINE
+            </a>
+          </li>
+          <li>
+            <a>
+              LOGIN
+            </a>
+          </li>
+        </ul>
+      </Nav>
+    </Header>
+
+    <Main>
+
+    </Main>
+
+    <Footer>
+
+    </Footer>
+
+    </>
   );
 }
 
